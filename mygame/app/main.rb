b3a71538872545a0d6f9167694983df0c2f35945
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-08-10 15:16:38 +0900

    Add antenna bounce frames

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,11 +1,11 @@
 require 'lib/screen'
 
-WALK_FRAMES = [1, 2, 3, 2].freeze
+WALK_FRAMES = [1, 2, 3, 4, 5, 6, 3, 4].freeze
 
 def tick(args)
   render_target = Screen.build_render_target(args, Screen::LOWREZJAM)
   render_target.background_color = { r: 37, g: 113, b: 121 }
-  frame = WALK_FRAMES[args.state.tick_count.idiv(16) % 4]
+  frame = WALK_FRAMES[args.state.tick_count.idiv(8) % WALK_FRAMES.size]
   render_target.sprites << {
     x: 20, y: 0, w: 32, h: 32,
     path: 'sprites/character.png',
