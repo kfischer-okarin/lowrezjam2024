1ae878b9dc3fb25352dc4e822eca5385d3487194
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-08-10 14:42:33 +0900

    Render rectangle on lowrez

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,5 +1,9 @@
+require 'lib/screen'
+
 def tick(args)
-  args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
-  args.outputs.labels  << [640, 420, 'Join the Discord! https://discord.dragonruby.org', 5, 1]
+  render_target = Screen.build_render_target(args, Screen::LOWREZJAM)
+  render_target.background_color = { r: 255, g: 255, b: 255 }
+  render_target.sprites << { x: 20, y: 20, w: 20, h: 20, path: :pixel, r: 255, g: 0, b: 0 }
+  args.outputs.background_color = { r: 0, g: 0, b: 0 }
+  args.outputs.sprites << Screen.sprite(Screen::LOWREZJAM)
 end
